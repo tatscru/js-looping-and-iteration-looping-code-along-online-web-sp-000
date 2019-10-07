98282155bbd388813f2c03fb02e7c5bea2b9f508
tatiana.mojica <tatiana.mojica@bustle.com>
2019-10-07 22:20:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
 // Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-  }
-
-  return gifts;
-}
-
-wrapGifts(gifts);
-
 
 function writeCards(name, event) {
   for (let i = 0; i < name.length; i++) {
