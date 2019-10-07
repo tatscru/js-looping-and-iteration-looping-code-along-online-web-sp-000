8d20e75d956dafcd0bcbf78b55ee9aae97a1162f
tatiana.mojica <tatiana.mojica@bustle.com>
2019-10-07 22:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function writeCards(name, event) {
   for (let i = 0; i < name.length; i++) {
     console.log(`Thank you, ${`name`[i]}, for the wonderful ${event} gift`);
   }
-  return writeCards(name,gift');
+  return writeCards(name,gift);
 }
